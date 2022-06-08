3bfeeebbbb40a595678b21537257aabb9d0d0bc1
Olexander753 <61043367+Olexander753@users.noreply.github.com>
2022-06-08 16:14:57 +0300

    Add files via upload

---
+++ derevo/derevo.cpp
@@ -0,0 +1,63 @@
+﻿#include <iostream>
+#include "Tree.h"
+
+int main()
+{
+/*
+	//обмен поддеревьев, тест успешный
+	Tree tree(1);
+	tree.insertData(&tree, 55);
+	tree.insertData(&tree, 33);
+	tree.insertData(&tree, 44);
+	tree.insertData(&tree, 7);
+	tree.insertData(&tree, 9);
+	tree.insertData(&tree, 14);
+	tree.insertData(&tree, 95);
+	tree.insertData(&tree, 94);
+
+	tree.printTree(&tree);
+
+	tree.testser(&tree, 33, 95);
+*/
+
+/*
+	//обмен поддеревьев, правое есть, а левого нет, тест неуспешный
+	Tree tree(1);
+	tree.insertData(&tree, 55);
+	tree.insertData(&tree, 33);
+	tree.insertData(&tree, 44);
+	tree.insertData(&tree, 7);
+	tree.insertData(&tree, 9);
+	tree.insertData(&tree, 14);
+	tree.insertData(&tree, 95);
+	tree.insertData(&tree, 94);
+
+	tree.printTree(&tree);
+
+	tree.testser(&tree, 6, 9);
+*/
+
+/*
+	//обмен поддеревьев, левое есть, а правого нет, тест неуспешный
+	Tree tree(1);
+	tree.insertData(&tree, 55);
+	tree.insertData(&tree, 33);
+	tree.insertData(&tree, 44);
+	tree.insertData(&tree, 7);
+	tree.insertData(&tree, 9);
+	tree.insertData(&tree, 14);
+	tree.insertData(&tree, 95);
+	tree.insertData(&tree, 94);
+
+	tree.printTree(&tree);
+
+	tree.testser(&tree, 94, 99);
+*/
+
+
+/*
+	//обмен поддеревьев в пустом дереве
+	Tree tree(1);
+	tree.testser(&tree, 6, 9);
+*/
+}
